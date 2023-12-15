d86189104784fcbb560b9ad4a8cdb61b0a13794a
admin <dsfasdafes@asdas.sd>
2023-12-15 16:20:16 +0500

    commit

--- utils/queue_state.py
+++ utils/queue_state.py
@@ -7,15 +7,13 @@ from data.user_model import User
 
 async def queue_state(bot: Bot):
     for user in session.query(User).all():
+        text = ''
         # states = requests.get('https://signal.umom.pro/get_state').json()
         states = requests.get('http://127.0.0.1:5000/get_state').json()
         # states = requests.get('https://signal-imitator.onrender.com').json()
         print(states)
         new_state_yellow, new_state_red = states.get('new_state_yellow', False), states.get('new_state_red', False)
         old_state_yellow, old_state_red = states.get('old_state_yellow', False), states.get('old_state_red', False)
-        text = markdown.text('Очереди практически нет. Можете смело спускаться в столовую!',
-                             'Приблизительное время в очереди: меньше 7ми минут.'
-                             )
         if new_state_yellow:
             text = markdown.text('Очереди средней длины. Возможно стоит сделать заказ в мобильном приложении.',
                                  'Приблизительное время в очереди: больше 7ми минут.'
@@ -24,5 +22,5 @@ async def queue_state(bot: Bot):
             text = markdown.text('Очередь очень длинная. Советуем сделать заказ в мобильном приложении.',
                                  'Приблизительное время в очереди: около 15ти минут.'
                                  )
-        if (old_state_yellow, old_state_red) != (new_state_yellow, new_state_red) or (new_state_yellow, new_state_red) == (False, False):
+        if (old_state_yellow, old_state_red) != (new_state_yellow, new_state_red) and text:
             await bot.send_message(chat_id=user.user_id, text=text)
